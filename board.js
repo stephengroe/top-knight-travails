5833b61223a90d8189b513c028c4d9a7a801bbe4
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-02 10:51:57 -0700

    Style knight

--- board.js
+++ board.js
@@ -9,7 +9,6 @@ function buildChessboard(dimensions) {
       const square = document.createElement("div");
       square.classList.add("square");
       square.dataset.coordinates = `${j},${i}`;
-      square.textContent = `${j},${i}`;
       if (lightColor) {
         square.classList.add("accent-color");
         lightColor = false;
@@ -27,24 +26,19 @@ function moveKnight(coordinates) {
   const board = document.querySelector("#chessboard");
   const square = document.querySelector(`[data-coordinates='${position}']`);
 
-  square.textContent = "here!";
-
   // Generate knight element
   const knight = document.querySelector("#knight");
 
-  // Set knight's size to same as a square
-  knight.style.height = `${square.offsetHeight}px`;
+  // Set knight's size and width to same as a square
+  knight.style.fontSize = `${square.offsetHeight}px`;
   knight.style.width = `${square.offsetWidth}px`;
 
   // Move based on square's location
   // (all relative, so works with window resizing!)
-  console.log(`x: ${coordinates[0]} / ${(dimensions[0])} = ${coordinates[0] / (dimensions[0])}`);
-  console.log(`y: ${coordinates[1]} / ${(dimensions[1])} = ${coordinates[1] / (dimensions[1])}`)
   const x = coordinates[0] / dimensions[0] * 100;
   const y = coordinates[1] / dimensions[1] * 100;
-  knight.style.left = `calc(${x}% - 5vmin)`;
+  knight.style.left = `${x}%`;
   knight.style.bottom = `${y}%`;
-  knight.textContent = position;
 };
 
 function getRandomCoordinate(dimensions) {
